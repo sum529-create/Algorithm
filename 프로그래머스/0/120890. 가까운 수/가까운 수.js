024be8b0ebc7b382afc6b1ba41272c3de68fbed9
sumin <72494319+sum529-create@users.noreply.github.com>
2025-02-17 09:43:24 +0900

    [level 0] Title: 가까운 수, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -0,0 +1,12 @@
+function solution(array, n) {
+    const sortArr = [...array].sort((a,b) => a-b)
+    const arr = sortArr.map(e => {
+        if(n === 0){
+            return Math.abs(1 - Math.abs(e))
+        }else {
+            return Math.abs(n - e)   
+        }
+    })
+    const idx = arr.indexOf(Math.min(...arr))
+    return [...array].sort((a,b) => a-b)[idx];
+}
