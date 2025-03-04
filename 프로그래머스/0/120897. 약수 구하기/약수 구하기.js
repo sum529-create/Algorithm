ab83c7a9d875d608e0037aae066c8e13bbbb22d9
sumin <72494319+sum529-create@users.noreply.github.com>
2025-03-04 10:01:12 +0900

    [level 0] Title: 약수 구하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let answer = [];
+    for (let i = 1; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            answer.push(i);
+            if (i !== n / i) {
+                answer.push(n / i);
+            }
+        }
+    }
+    return answer.sort((a, b) => a - b);
+}
