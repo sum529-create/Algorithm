0f035cce6c3889b170584f3b97dd7ed57f3ebd15
sumin <72494319+sum529-create@users.noreply.github.com>
2025-07-15 12:45:39 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 9356 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+if(input.includes('-')){
+  const numArr = input.replaceAll('-', ' ').split(' ').map(e => {
+     if(e.includes('+'))
+      return e.split('+').map(Number).reduce((a,c) => a+c);
+     else return Number(e)
+  }) 
+  console.log(numArr.reduce((a,c) => a-c))
+} else {
+    console.log(input.split('+').map(Number).reduce((a,c) => a+c));
+}
