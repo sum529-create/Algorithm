7887c299442dce36977339969fa4f20222f00e2e
sumin <72494319+sum529-create@users.noreply.github.com>
2025-07-15 12:48:01 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const input = fs.readFileSync('/dev/stdin').toString().trim();
 
 if(input.includes('-')){
-  const numArr = input.replaceAll('-', ' ').split(' ').map(e => {
+  const numArr = input.split('-').map(e => {
      if(e.includes('+'))
       return e.split('+').map(Number).reduce((a,c) => a+c);
      else return Number(e)
